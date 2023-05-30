f354e073c5c9671a4c09b016b65c57890e939852
Bart Otten <bart.otten85@gmail.com>
2023-05-30 18:41:07 +0200

    docs: restyle 'use' nutrition facts

--- lib/routex.ex
+++ lib/routex.ex
@@ -1,27 +1,30 @@
 defmodule Routex do
   @moduledoc """
-  When use'd this module generates a Routext backend and a configuration struct using
-  the `configure/2` callbacks of the extensions provided in `opts`.
-
-  See also: [Routex Extensions](EXTENSIONS.md).
+  > #### `use Routex` {: .info}
+  > When use'd this module generates a Routext backend module and
+  > a configuration struct using  the `configure/2` callbacks of
+  > the extensions provided in `opts`.
+  >
+  > See also: [Routex Extensions](EXTENSIONS.md).
 
   **Example**
 
-      iex> defmodule MyApp.RtxBackend do
-      ...>  use Routex,
-      ...>   extensions: [
-      ...>    Routex.Extension.VerifiedRoutes,
-      ...>    Routex.Extension.AttrGetters,
-      ...>   ],
-      ...>   bar: [some_opts: "value"]
-      ...> end
-      iex> IO.inspect(%MyApp.RtxBackend{})
-      %MyApp.RtxBackend{
-        bar: [some_opts: "value"],
-        extensions: [Routex.Extension.VerifiedRoutes, Routex.Extension.AttrGetters],
-        verified_sigil_routex: "~l",
-        verified_sigil_original: "~o"
-      }
+       iex> defmodule MyApp.RtxBackend do
+       ...>  use Routex,
+       ...>   extensions: [
+       ...>    Routex.Extension.VerifiedRoutes,
+       ...>    Routex.Extension.AttrGetters,
+       ...>   ],
+       ...>   bar: [some_opts: "value"]
+       ...> end
+       iex> IO.inspect(%MyApp.RtxBackend{})
+       %MyApp.RtxBackend{
+         bar: [some_opts: "value"],
+         extensions: [Routex.Extension.VerifiedRoutes, Routex.Extension.AttrGetters],
+         verified_sigil_routex: "~l",
+         verified_sigil_original: "~o"
+       }
+
   """
 
   alias Routex.Processing
@@ -31,6 +34,7 @@ defmodule Routex do
   """
   @type t :: module
 
+  @spec __using__(opts :: list) :: Macro.output()
   defmacro __using__(opts) do
     opts = process_opts(opts, __CALLER__)
     extensions = Keyword.get(opts, :extensions, [])

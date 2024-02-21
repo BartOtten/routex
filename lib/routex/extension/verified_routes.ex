cb6da3d9bc0b395721a53b878d135b03c019a0cf
Bart Otten <bart.otten85@gmail.com>
2024-02-22 00:34:16 +0100

    wip(verified): support url/{1,2,3} and path/{2,3}

--- lib/routex/extension/verified_routes.ex
+++ lib/routex/extension/verified_routes.ex
@@ -108,72 +108,250 @@ defmodule Routex.Extension.VerifiedRoutes do
       end)
       |> Map.new()
 
-    original_sigil =
-      if verified_sigil_routex == @phoenix_sigil do
-        "~" <> sigil_letter = verified_sigil_original
-        sigil_fun_name = String.to_atom("sigil_" <> sigil_letter)
+    to_sigil_funname = fn "~" <> sigil_letter -> String.to_atom("sigil_" <> sigil_letter) end
+    org_sigil_fun_name = to_sigil_funname.(verified_sigil_original)
+    routex_sigil_fun_name = to_sigil_funname.(verified_sigil_routex)
+    phoenix_sigil_fun_name = to_sigil_funname.(@phoenix_sigil)
 
+    original_sigil_ast =
+      if verified_sigil_routex == @phoenix_sigil do
         quote location: :keep do
-          defmacro unquote(sigil_fun_name)({:<<>>, meta, segments} = route, extra) do
+          defmacro unquote(org_sigil_fun_name)(route, flags) do
             quote location: :keep do
-              Phoenix.VerifiedRoutes.sigil_p(unquote(route), unquote(extra))
+              Phoenix.VerifiedRoutes.sigil_p(unquote(route), unquote(flags))
             end
           end
         end
       end
 
-    localized_sigil =
+    routex_sigil_ast =
       quote location: :keep do
-        defmacro sigil_p(route, extra) do
-          Routex.Extension.VerifiedRoutes.sigil_callback(
+        defmacro unquote(routex_sigil_fun_name)(route, flags) do
+          callback =
+            fn reroute ->
+              quote location: :keep do
+                Phoenix.VerifiedRoutes.sigil_p(unquote(reroute), unquote(flags))
+              end
+            end
+
+          Routex.Extension.VerifiedRoutes.branch_callback(
             route,
-            extra,
             unquote(Macro.escape(pattern_routes)),
-            __CALLER__
+            __CALLER__,
+            callback
           )
         end
       end
 
-    [original_sigil, localized_sigil]
+    # create drop-in variants of Phoenix.VerifiedRoutes.url/{1,2,3} for custom 'original' sigil
+    original_url_ast =
+      quote location: :keep do
+        defmacro url({unquote(org_sigil_fun_name), meta, opts}) do
+          quote location: :keep, do: Phoenix.VerifiedRoutes.url(unquote({:sigil_p, meta, opts}))
+        end
+
+        defmacro url(conn_or_endpoint, {unquote(org_sigil_fun_name), meta, opts}) do
+          quote location: :keep,
+                do: Phoenix.VerifiedRoutes.url(unquote(conn_or_endpoint, {:sigil_p, meta, opts}))
+        end
+
+        defmacro url(conn_or_endpoint, router, {unquote(org_sigil_fun_name), meta, opts}) do
+          quote location: :keep,
+                do:
+                  Phoenix.VerifiedRoutes.url(
+                    unquote(conn_or_endpoint, router, {:sigil_p, meta, opts})
+                  )
+        end
+      end
+
+    # create drop-in variants of Phoenix.VerifiedRoutes.path/{2,3} for custom 'original' sigil
+    original_path_ast =
+      quote location: :keep do
+        defmacro path(conn_or_endpoint, router, {unquote(org_sigil_fun_name), meta, opts}) do
+          quote location: :keep,
+                do:
+                  Phoenix.VerifiedRoutes.path(
+                    unquote(conn_or_endpoint, router, {:sigil_p, meta, opts})
+                  )
+        end
+
+        defmacro path(conn_or_endpoint, {unquote(org_sigil_fun_name), meta, opts}) do
+          quote location: :keep,
+                do: Phoenix.VerifiedRoutes.url(unquote(conn_or_endpoint, {:sigil_p, meta, opts}))
+        end
+      end
+
+    # branching url/[2,3,4]
+    branching_url_ast =
+      quote location: :keep do
+        defmacro url(route) do
+          router = router = attr!(__CALLER__, :router)
+          endpoint = attr!(__CALLER__, :endpoint)
+          quote do: url(unquote(endpoint), unquote(router), unquote(route))
+        end
+
+        defmacro url(endpoint, route) do
+          router = attr!(__CALLER__, :router)
+          quote do: url(unquote(endpoint), unquote(router), unquote(route))
+        end
+
+        defmacro url(
+                   endpoint,
+                   router,
+                   {unquote(routex_sigil_fun_name), meta, [route | rest] = opts}
+                 ) do
+          callback = fn reroute ->
+            quote location: :keep do
+              Phoenix.VerifiedRoutes.url(
+                unquote(endpoint),
+                unquote(router),
+                sigil_p(unquote(reroute), unquote(rest))
+              )
+            end
+          end
+
+          Routex.Extension.VerifiedRoutes.branch_callback(
+            route,
+            unquote(Macro.escape(pattern_routes)),
+            __CALLER__,
+            callback
+          )
+        end
+      end
+
+    # branching path/[2,3]
+    branching_path_ast =
+      quote location: :keep do
+        defmacro path(endpoint, route) do
+          router = attr!(__CALLER__, :router)
+          quote do: path(unquote(endpoint), unquote(router), unquote(route))
+        end
+
+        defmacro path(
+                   endpoint,
+                   router,
+                   {unquote(routex_sigil_fun_name), meta, [route | rest] = opts}
+                 ) do
+          callback = fn reroute ->
+            quote location: :keep do
+              Phoenix.VerifiedRoutes.path(
+                unquote(endpoint),
+                unquote(router),
+                sigil_p(unquote(reroute), unquote(rest))
+              )
+            end
+          end
+
+          Routex.Extension.VerifiedRoutes.branch_callback(
+            route,
+            unquote(Macro.escape(pattern_routes)),
+            __CALLER__,
+            callback
+          )
+        end
+      end
+
+    url_catchall_ast =
+      quote do
+        defmacro url(other), do: raise_invalid_route(other)
+
+        defmacro url(_conn_or_socket_or_endpoint_or_uri, other),
+          do: raise_invalid_route(other)
+
+        defmacro url(_conn_or_socket_or_endpoint_or_uri, _router, other),
+          do: raise_invalid_route(other)
+      end
+
+    path_catchall_ast =
+      quote do
+        defmacro path(_conn_or_socket_or_endpoint_or_uri, other),
+          do: raise_invalid_route(other)
+
+        defmacro path(_conn_or_socket_or_endpoint_or_uri, _router, other),
+          do: raise_invalid_route(other)
+      end
+
+    phoenix_clones_ast =
+      quote do
+        # Clones of private functions in Phoenix.VerifiedRoutes
+
+        defp raise_invalid_route(ast) do
+          raise ArgumentError,
+                "expected compile-time ~l path string, got: #{Macro.to_string(ast)}\n" <>
+                  "Use unverified_path/2 and unverified_url/2 if you need to build an arbitrary path."
+        end
+
+        # unused; kept for clearity
+        # defp attr!(%{function: nil}, _) do
+        #   raise "Phoenix.VerifiedRoutes can only be used inside functions, please move your usage of ~p to functions"
+        # end
+
+        defp attr!(env, :endpoint) do
+          Module.get_attribute(env.module, :endpoint) ||
+            raise """
+            expected @endpoint to be set. For dynamic endpoint resolution, use path/2 instead.
+
+            for example:
+
+                path(conn_or_socket, ~p"/my-path")
+            """
+        end
+
+        defp attr!(env, name) do
+          Module.get_attribute(env.module, name) ||
+            raise "expected @#{name} module attribute to be set"
+        end
+      end
+
+    [
+      original_sigil_ast,
+      routex_sigil_ast,
+      original_url_ast,
+      branching_url_ast,
+      url_catchall_ast,
+      original_path_ast,
+      branching_path_ast,
+      path_catchall_ast,
+      phoenix_clones_ast
+    ]
   end
 
   @doc false
-  def sigil_callback(route, extra, pattern_routes, caller) do
+  def branch_callback(route, pattern_routes, caller, callback) do
     {:<<>>, _meta, segments} = route
     pattern = Path.to_match_pattern(segments)
     routes_matching_pattern = Map.get(pattern_routes, pattern, [])
 
-    # Routex does not handle all routes. Return the original route if we find
+    # Routex does not handle all routes. Use the fallback if we find
     # none handled by Routex.
     if routes_matching_pattern === [] do
       quote do
-        Phoenix.VerifiedRoutes.sigil_p(unquote(route), unquote(extra))
+        unquote(callback.(route))
       end
     else
-      build_case(segments, routes_matching_pattern, caller)
+      clauses = build_branches(segments, routes_matching_pattern, callback)
+      build_tree(segments, clauses, caller)
     end
   end
 
-  defp build_case(segments, routes_matching_pattern, caller) do
-    cases = build_case_clauses(segments, routes_matching_pattern)
+  defp build_tree(segments, clauses, caller) do
     helper_ast = ExtensionUtils.get_helper_ast(caller)
 
     quote do
       case {unquote(Macro.escape(segments)), unquote(helper_ast)} do
-        unquote(cases)
+        unquote(clauses)
       end
     end
   end
 
-  defp build_case_clauses(segments, routes_matching_pattern) do
+  defp build_branches(segments, routes_matching_pattern, callback) do
     for route <- routes_matching_pattern do
       new_segments = route |> Attrs.get(:__origin__) |> Path.recompose(route.path, segments)
-
       helper = route |> Attrs.get(:__order__) |> List.last()
 
       quote do
         {unquote(Macro.escape(segments)), unquote(helper)} ->
-          Phoenix.VerifiedRoutes.sigil_p(<<unquote_splicing(new_segments)>>, [])
+          unquote(callback.(quote do: <<unquote_splicing(new_segments)>>))
       end
     end
     |> List.flatten()

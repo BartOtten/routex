16faf547fdc3a809657a24c5c97babeedd4938e1
Bart Otten <bart.otten85@gmail.com>
2024-11-04 21:54:47 +0100

    chore(helpers): add test for build_case

--- test/routex/extension/route_helpers_test.exs
+++ test/routex/extension/route_helpers_test.exs
@@ -1,43 +1,42 @@
 defmodule Routex.Extension.RouteHelpersTest do
   use ExUnit.Case
 
+  @routes [
+    %Phoenix.Router.Route{
+      helper: "home",
+      path: "/",
+      verb: :get,
+      private: %{routex: %{__origin__: "/", __branch__: [0, 0]}}
+    },
+    %Phoenix.Router.Route{
+      helper: "home_alt1",
+      path: "/alt1",
+      verb: :get,
+      private: %{routex: %{__origin__: "/", __branch__: [0, 0, 1]}}
+    },
+    %Phoenix.Router.Route{
+      helper: "home_alt2",
+      path: "/alt2",
+      verb: :get,
+      private: %{routex: %{__origin__: "/", __branch__: [0, 0, 2]}}
+    },
+    %Phoenix.Router.Route{
+      helper: "products",
+      path: "/products/:id",
+      verb: :get,
+      private: %{routex: %{__origin__: "/products/:id", __branch__: [0, 1]}}
+    },
+    %Phoenix.Router.Route{
+      helper: "posts",
+      path: "/posts/:id",
+      verb: :get,
+      private: %{routex: %{__origin__: "/posts/:id", __branch__: [0, 2]}}
+    }
+  ]
   alias Routex.Extension.RouteHelpers
 
   test "creates helper functions" do
-    routes = [
-      %Phoenix.Router.Route{
-        helper: "home",
-        path: "/",
-        verb: :get,
-        private: %{routex: %{__origin__: "/", __branch__: [0, 0]}}
-      },
-      %Phoenix.Router.Route{
-        helper: "home_alt1",
-        path: "/alt1",
-        verb: :get,
-        private: %{routex: %{__origin__: "/", __branch__: [0, 0, 1]}}
-      },
-      %Phoenix.Router.Route{
-        helper: "home_alt2",
-        path: "/alt2",
-        verb: :get,
-        private: %{routex: %{__origin__: "/", __branch__: [0, 0, 2]}}
-      },
-      %Phoenix.Router.Route{
-        helper: "products",
-        path: "/products/:id",
-        verb: :get,
-        private: %{routex: %{__origin__: "/products/:id", __branch__: [0, 1]}}
-      },
-      %Phoenix.Router.Route{
-        helper: "posts",
-        path: "/posts/:id",
-        verb: :get,
-        private: %{routex: %{__origin__: "/posts/:id", __branch__: [0, 2]}}
-      }
-    ]
-
-    ast = RouteHelpers.create_helpers(routes, nil, __ENV__)
+    ast = RouteHelpers.create_helpers(@routes, nil, __ENV__)
 
     {:module, module, _, _} = Module.create(HelpersModule, ast, __ENV__)
 
@@ -55,5 +54,68 @@ defmodule Routex.Extension.RouteHelpersTest do
       ]
 
     assert expected == module.__info__(:functions)
+  end
+
+  test "build_case" do
+    expected =
+      {
+        :case,
+        [],
+        [
+          {
+            :if,
+            [context: Routex.Utils, imports: [{2, Kernel}]],
+            [
+              {:=, [],
+               [
+                 {:branch, [], Routex.Utils},
+                 {{:., [], [{:__aliases__, [alias: false], [:Process]}, :get]}, [], [:rtx_branch]}
+               ]},
+              [do: {:branch, [], Routex.Utils}, else: 0]
+            ]
+          },
+          [
+            do: [
+              {:->, [],
+               [
+                 [0],
+                 {:apply,
+                  [context: Routex.Extension.RouteHelpers, imports: [{2, Kernel}, {3, Kernel}]],
+                  [MyRouter.Helpers, :home_suffix, []]}
+               ]},
+              {:->, [],
+               [
+                 [1],
+                 {:apply,
+                  [context: Routex.Extension.RouteHelpers, imports: [{2, Kernel}, {3, Kernel}]],
+                  [MyRouter.Helpers, :home_alt1_suffix, []]}
+               ]},
+              {:->, [],
+               [
+                 [2],
+                 {:apply,
+                  [context: Routex.Extension.RouteHelpers, imports: [{2, Kernel}, {3, Kernel}]],
+                  [MyRouter.Helpers, :home_alt2_suffix, []]}
+               ]},
+              {:->, [],
+               [
+                 [1],
+                 {:apply,
+                  [context: Routex.Extension.RouteHelpers, imports: [{2, Kernel}, {3, Kernel}]],
+                  [MyRouter.Helpers, :products_suffix, []]}
+               ]},
+              {:->, [],
+               [
+                 [2],
+                 {:apply,
+                  [context: Routex.Extension.RouteHelpers, imports: [{2, Kernel}, {3, Kernel}]],
+                  [MyRouter.Helpers, :posts_suffix, []]}
+               ]}
+            ]
+          ]
+        ]
+      }
+
+    assert expected == RouteHelpers.build_case([[__ENV__, @routes, MyRouter, "_suffix"], []])
   end
 end

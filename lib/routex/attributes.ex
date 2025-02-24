bfc67ae2a9e41f2a08135b2c5ea698bd54a41c7c
Bart Otten <bart.otten85@gmail.com>
2025-02-24 02:33:29 +0100

    refactor: use Attrs.get!/2 instead of struct field access

--- lib/routex/attributes.ex
+++ lib/routex/attributes.ex
@@ -1,35 +1,54 @@
 defmodule Routex.Attrs do
   @moduledoc """
-  Provides an interface to access and update Routex attributes.
+  Provides an interface to access and update Routex attributes
+  in routes, sockets, or connections (hereinafter `containers`).
 
   Extensions can make use of `Routex.Attrs` values provided by Routex itself,
-  Routex backends and other extensions. As those values are attributes to a
-  route, extension B can use values attributed to a route by extension A.
+  Routex backends, and other extensions. As these values are attributes to a route,
+  one extension can use values set by another.
 
-  > #### In combination with... {: .neutral}
-  > Other extensions set `Routex.Attrs`. The attributes an extension sets is listed in it's documentation.
-  > To define custom attributes for routes have a look at `Routex.Extension.Alternatives`
+  Other extensions set `Routex.Attrs` (see each extension’s documentation for the
+  list of attributes they set). To define custom attributes for routes, see
+  `Routex.Extension.Alternatives`.
 
-  * To make the availability of the attributes as predictable as possible, Routex
-  uses a flat structure.
-  * Extension developers are encouraged to put as much information into the attributes
-  as possible.
-  * Extensions should add any fallback/default they might use themselves to the
-  attributes.
+  * To ensure predictable availability, Routex uses a flat structure.
+  * Extension developers are encouraged to embed as much contextual information as possible.
+  * Extensions should add any fallback/default they might use to the attributes.
   """
+
+  @type container :: %{private: map()}
+  @type key :: atom()
+  @type value :: any()
+  @type attrs_fun :: (map() -> Enumerable.t())
+  @type update_fun :: (value() -> value())
 
   @doc """
-  Returns true when the provided key or attribute is private.
+  Returns true if the given key or attribute tuple represents a private attribute.
+
+  A private attribute is one whose name starts with `"__"`.
   """
-  @spec private?({atom, any} | atom) :: boolean()
+  @spec private?({atom(), any()} | atom()) :: boolean()
   def private?({key, _v}), do: private?(key)
-  def private?(key), do: key |> Atom.to_string() |> String.starts_with?("__")
+
+  def private?(key) when is_atom(key) do
+    key |> Atom.to_string() |> String.starts_with?("__")
+  end
 
   @doc """
-  Removes non private fields from attrs. Input is either an opts map or a `Route`.
+  Removes non-private fields from attributes.
+
+  When given a plain map, it filters the map to include only keys starting with `"__"`.
+  When given a container (a map with a `:private` key), it filters the `:routex` attributes
+  in the private map.
   """
-  def cleanup(%Phoenix.Router.Route{} = route) do
-    update_in(route, [Access.key!(:private), :routex], fn x -> Map.filter(x, &private?/1) end)
+  @spec cleanup(map() | container()) :: map() | container()
+  def cleanup(route_sock_or_conn) when is_struct(route_sock_or_conn) do
+    route_sock_or_conn
+    |> ensure_routex()
+    |> update_in(
+      [Access.key!(:private), :routex],
+      &Map.filter(&1, fn kv -> private?(kv) end)
+    )
   end
 
   def cleanup(meta) when is_map(meta) do
@@ -37,79 +56,123 @@ defmodule Routex.Attrs do
   end
 
   @doc """
-  Updates the options of a `route` by applying given `function`.
+  Updates the container's attributes by applying the given function.
+
+  The function receives the current attributes map and must return an enumerable,
+  which is then converted into a new map.
   """
-  def update(route, fun) do
-    current = get(route)
+  @spec update(container(), attrs_fun()) :: container()
+  def update(route_sock_or_conn, fun) when is_function(fun, 1) do
+    current = get(route_sock_or_conn)
     new = current |> fun.() |> Enum.into(%{})
-    put(route, new)
+    put(route_sock_or_conn, new)
   end
 
   @doc """
-  Updates the value assigned to `key` of a `route`'s options by applying given `function`.
+  Updates the value assigned to `key` in the container's attributes by applying the given function.
   """
-  def update(route, key, fun) do
-    current = get(route, key)
+  @spec update(container(), key(), update_fun()) :: container()
+  def update(route_sock_or_conn, key, fun) when is_function(fun, 1) do
+    current = get(route_sock_or_conn, key)
     new = fun.(current)
-    put(route, key, new)
+    put(route_sock_or_conn, key, new)
   end
 
   @doc """
-  Merge `value` into `route`'s options.
+  Merges the given value into the container's attributes.
+
+  The value can be either a list of key-value pairs or a map.
   """
+  @spec merge(container(), keyword() | map()) :: container()
+  def merge(route_sock_or_conn, value) when is_list(value) do
+    merge(route_sock_or_conn, Map.new(value))
+  end
 
-  def merge(route, value) when is_list(value),
-    do: merge(route, Map.new(value))
-
-  def merge(route, value) when is_map(value),
-    do: Enum.reduce(value, route, fn {k, v}, acc -> put(acc, k, v) end)
+  def merge(route_sock_or_conn, value) when is_map(value) do
+    Enum.reduce(value, route_sock_or_conn, fn {k, v}, acc ->
+      put(acc, k, v)
+    end)
+  end
 
   @doc """
-  Assigns `value` to `route`'s options.
+  Replaces the container's attributes with the provided map.
   """
-
-  def put(route, value) when is_map(value),
-    do: %{route | private: Map.put(route.private || %{}, :routex, value)}
+  @spec put(container(), map()) :: container()
+  def put(route_sock_or_conn, value) when is_map(value) do
+    route_sock_or_conn
+    |> ensure_routex()
+    |> put_in([Access.key!(:private), :routex], value)
+  end
 
   @doc """
-  Assigns `value` to `key` in `route`'s options.
+  Assigns `value` to `key` in the container's attributes.
   """
-  def put(route, key, value) when is_atom(key) and is_map_key(route.private, :routex),
-    do: %{
-      route
-      | private: %{route.private | routex: Map.put(route.private.routex, key, value)}
-    }
-
-  def put(route, key, value) when is_atom(key),
-    do: put(%{route | private: Map.put(route.private || %{}, :routex, %{})}, key, value)
+  @spec put(container(), key(), value()) :: container()
+  def put(route_sock_or_conn, key, value) when is_atom(key) do
+    route_sock_or_conn
+    |> ensure_routex()
+    |> update_in([Access.key!(:private), :routex], &Map.put(&1, key, value))
+  end
 
   @doc """
-  Get the value assigned to `key` from `route`'s options or return `default`. When no
-  key is provided, the function returns all options.
+  Retrieves the value for `key` from the container's attributes, or returns `default`.
+
+  When no key is provided, returns the entire attributes map.
   """
-  def get(route, key \\ nil, default \\ nil)
+  @spec get(container(), key() | nil, value()) :: value() | map()
+  def get(route_sock_or_conn, key \\ nil, default \\ nil)
 
-  def get(route, nil, _default), do: route.private.routex
-
-  def get(route, key, default)
-      when is_atom(key) and is_map_key(route.private, :routex),
-      do: Map.get(route.private.routex, key, default)
-
-  def get(_route, key, default)
-      when is_atom(key),
-      do: default
-
-  @doc """
-  Get the value assigned to `key` from `route`'s options or raise if `key` not found. Accepts an optional
-  custom `error_msg` as third argument
-  """
-  def get!(route, key, error_msg \\ nil) do
-    error_msg = error_msg || "Key #{inspect(key)} not found in #{inspect(route.private.routex)}"
-
-    try do
-      Map.fetch!(route.private.routex, key)
-    rescue
-      _e -> reraise(error_msg, __STACKTRACE__)
+  def get(route_sock_or_conn, nil, _default) do
+    case route_sock_or_conn.private do
+      %{routex: attrs} -> attrs
+      _ -> %{}
     end
+  end
+
+  def get(route_sock_or_conn, key, default) when is_atom(key) do
+    case route_sock_or_conn.private do
+      %{routex: attrs} -> Map.get(attrs, key, default)
+      _ -> default
+    end
+  end
+
+  @doc """
+  Retrieves the value for `key` from the container's attributes.
+
+  Raises an error (with an optional custom message) if the key is not found.
+  """
+  @spec get!(container(), key(), String.t() | nil) :: value() | no_return()
+  def get!(route_sock_or_conn, key, error_msg \\ nil) when is_atom(key) do
+    attrs =
+      case route_sock_or_conn.private do
+        %{routex: attrs} -> attrs
+        _ -> %{}
+      end
+
+    case Map.fetch(attrs, key) do
+      {:ok, value} ->
+        value
+
+      :error ->
+        msg =
+          error_msg ||
+            "Key #{inspect(key)} not found in #{inspect(attrs)}"
+
+        raise(msg)
+    end
+  end
+
+  # Ensures that the container has a :routex key in its :private map.
+  @spec ensure_routex(container()) :: container()
+  defp ensure_routex(%{private: %{}} = route_sock_or_conn) do
+    update_in(route_sock_or_conn, [Access.key(:private, %{})], fn private ->
+      Map.put_new(private, :routex, %{})
+    end)
+  end
+
+  defp ensure_routex(%{private: nil} = route_sock_or_conn) do
+    route_sock_or_conn
+    |> Map.put(:private, %{})
+    |> ensure_routex()
   end
 end

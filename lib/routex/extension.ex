67c17141e6eed267b762f38f193dd34c5f045ca6
Bart Otten <bart.otten85@gmail.com>
2025-02-26 16:31:54 +0100

    feat: support LiveView livecycle hooks and Plugs in extensions

--- lib/routex/extension.ex
+++ lib/routex/extension.ex
@@ -2,19 +2,28 @@ defmodule Routex.Extension do
   @moduledoc """
   Specification for composable Routex extensions.
 
-  Optional callbacks:
-  - configure
-  - transform
-  - post_transform
-  - create_helpers
+  All callbacks are *optional*
 
-  See also: [Routex Extensions](EXTENSIONS.md)
+  See also: [Routex Extensions](EXTENSION_DEVELOPMENT.md)
   """
 
   @type backend :: Routex.Backend.t()
   @type env :: Macro.Env.t()
   @type opts :: list
   @type routes :: [Phoenix.Router.Route.t()]
+  @type params :: map
+  @type url :: binary()
+  @type socket :: Phoenix.Socket.t()
+  @type conn :: Plug.Conn.t()
+  @type attrs :: map
+
+  @supported_livecycle_stages [
+    handle_params: 4,
+    handle_event: 4,
+    handle_info: 4,
+    handle_async: 4,
+    after_render: 4
+  ]
 
   @doc """
   The `configure/2` callback is called in the first stage with the options
@@ -39,6 +48,23 @@ defmodule Routex.Extension do
   """
   @callback post_transform(routes, backend, env) :: routes
 
+  for {stage, _arity} <- @supported_livecycle_stages do
+    @doc ~s"""
+    Callback for the `#{stage}` livecycle stage with the same name. Receives an
+    additional argument `attrs` with `Routex.Attrs` of the current route. See
+    also [Livecycle callbacks](#livecycle-callbacks)
+
+    The callback should return either `{:cont, socket}` or `{:halt, socket}`
+    """
+    @callback unquote(stage)(params, url, socket, attrs) :: {:cont, %Phoenix.Socket{}}
+  end
+
+  @doc ~s"""
+  Callback for the Plug pipeline. Receives an additional argument `attrs` with
+  `Routex.Attrs` of the current route.  The callback should return `conn`
+  """
+  @callback plug(conn, opts, attrs) :: conn
+
   @doc """
   The `create_helpers/3` callback is called in the last stage with a list of
   routes belonging to a Routex backend, the name of the Routex backend and
@@ -48,5 +74,6 @@ defmodule Routex.Extension do
   """
   @callback create_helpers(routes, backend, env) :: Macro.output()
 
-  @optional_callbacks transform: 3, create_helpers: 3, configure: 2, post_transform: 3
+  @optional_callbacks [configure: 2, transform: 3, post_transform: 3, create_helpers: 3, plug: 3] ++
+                        @supported_livecycle_stages
 end

8f62e40c4b685a2133b69ac91f10635e8f3a4b4c
Bart Otten <bart.otten85@gmail.com>
2025-02-26 16:35:53 +0100

    docs: minor fixes
    - add warning to github
    - remove unreleased callbacks
    - clarify wording assigns extensions
    - add doc to generated plug

--- lib/routex/extension/assigns.ex
+++ lib/routex/extension/assigns.ex
@@ -9,8 +9,10 @@ defmodule Routex.Extension.Assigns do
 
 
   ## Options
-  - `namespace`: when set creates a named collection of `Routex.Attrs`
-  - `attrs`: when set defines keys of `Routex.Attrs` to make available
+  - `namespace`: when set creates a named collection: assigns available as @namespace.key
+  - `attrs`: If attrs is not set, all Routex.Attrs are included. If attrs is set
+    to a list of keys, only the specified subset of attributes will be
+    available.
 
   ## Configuration
   ```diff
@@ -73,7 +75,6 @@ defmodule Routex.Extension.Assigns do
   @doc """
   Hook attached to the `handle_params` stage in the LiveView life cycle
   """
-  @impl Routex.Extension
   def handle_params(_params, _uri, socket, attrs \\ %{}) do
     assigns = Map.get(attrs, :assigns, %{})
     socket = Phoenix.Component.assign(socket, assigns)
@@ -84,7 +85,6 @@ defmodule Routex.Extension.Assigns do
   @doc """
   Plug added to the Plug pipeline
   """
-  @impl Routex.Extension
   def plug(conn, _opts, attrs \\ %{}) do
     assigns = Map.get(attrs, :assigns, [])
     Plug.Conn.merge_assigns(conn, assigns)

d90e877ee4789f06bd2564c4b5dd9be3cfd17706
Bart Otten <bart.otten85@gmail.com>
2025-03-27 14:46:10 +0100

    refactor: ensure compatibility with the current major version

--- lib/routex/helper_fallbacks.ex
+++ lib/routex/helper_fallbacks.ex
@@ -3,7 +3,7 @@ defmodule Routex.HelperFallbacks do
   Provides fallback functions when `use`'d
   """
   defmacro __using__(_) do
-    quote do
+    quote generated: true do
       @doc "Fallback for attrs/1 returning an empty map."
       @spec attrs(url :: binary()) :: no_return()
       def attrs(url),
@@ -19,13 +19,57 @@ defmodule Routex.HelperFallbacks do
           https://hexdocs.pm/routex/Routex.Extension.AttrGetters.html
           """)
 
-      @doc "Fallback for on_mount returning `{:cont, socket}` with unmodified socket"
+      @doc "Fallback for on_mount. Assigns :url"
       @spec on_mount(atom(), map(), map(), Phoenix.Socket.t()) :: {:cont, Phoenix.Socket.t()}
-      def on_mount(_key, _params, _session, socket), do: {:cont, socket}
+      def on_mount(_key, _params, _session, socket) do
+        {:cont,
+         Phoenix.LiveView.attach_hook(
+           socket,
+           :rtx_fallback,
+           :handle_params,
+           &__MODULE__.fallback_handle_params/3
+         )}
+      end
 
-      @doc "Fallback for plug/2 returning the conn unmodified"
+      # credo:disable-for-next-line
+      # TODO: Remove in next major version.
+      {:ok, phx_version} = :application.get_key(:phoenix, :vsn)
+
+      if phx_version |> to_string() |> Version.match?("< 1.7.0-dev") do
+        @assign_mod Phoenix.LiveView
+      else
+        @assign_mod Phoenix.Component
+      end
+
+      @spec fallback_handle_params(map(), binary(), Phoenix.LiveView.Socket.t()) ::
+              {:cont, Phoenix.LiveView.Socket.t()}
+      def fallback_handle_params(_params, url, socket) do
+        attrs = attrs(url)
+
+        socket =
+          socket
+          |> Routex.Attrs.merge(%{url: url, __branch__: attrs.__branch__})
+          |> @assign_mod.assign(url: url)
+          |> @assign_mod.assign(attrs[:assigns] || %{})
+
+        {:cont, socket}
+      end
+
+      @doc "Fallback for plug/2. Assigns :url"
       @spec plug(Plug.Conn.t(), keyword()) :: Plug.Conn.t()
-      def plug(conn, _opts), do: conn
+      def plug(conn, _opts) do
+        url =
+          case {conn.request_path, conn.query_string} do
+            {path, ""} -> path
+            {path, query} -> "#{path}?#{query}"
+          end
+
+        attrs = attrs(url)
+
+        conn
+        |> Routex.Attrs.merge(%{url: url, __branch__: attrs.__branch__})
+        |> Plug.Conn.assign(:url, url)
+      end
 
       defoverridable attrs: 1, on_mount: 4, plug: 2
     end

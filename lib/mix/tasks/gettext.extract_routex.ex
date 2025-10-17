f0864bde66061519d0e0376813fdb70f64640ace
Bart Otten <bart.otten85@gmail.com>
2025-10-17 22:01:18 +0200

    chore: enhanced compilation reliability

--- lib/mix/tasks/gettext.extract_routex.ex
+++ lib/mix/tasks/gettext.extract_routex.ex
@@ -1,22 +1,24 @@
-defmodule Mix.Tasks.Gettext.Extract.Routex do
-  @shortdoc "Force translatable segments extraction"
-  @moduledoc """
-  A Mix task to force extraction of Gettext translatable segments.
-  """
-  use Mix.Task
-  @recursive false
+if Code.ensure_loaded?(Gettext) do
+  defmodule Mix.Tasks.Gettext.Extract.Routex do
+    @shortdoc "Force translatable segments extraction"
+    @moduledoc """
+    A Mix task to force extraction of Gettext translatable segments.
+    """
+    use Mix.Task
+    @recursive false
 
-  def run(args) do
-    _status = Application.ensure_all_started(:gettext)
-    force_recompile_with_extractor()
+    def run(args) do
+      _status = Application.ensure_all_started(:gettext)
+      force_recompile_with_extractor()
 
-    Mix.Task.run("gettext.extract", args)
-  end
+      Mix.Task.run("gettext.extract", args)
+    end
 
-  defp force_recompile_with_extractor do
-    Gettext.Extractor.enable()
-    Mix.Task.run("compile", ["--force"])
-  after
-    Gettext.Extractor.disable()
+    defp force_recompile_with_extractor do
+      Gettext.Extractor.enable()
+      Mix.Task.run("compile", ["--force"])
+    after
+      Gettext.Extractor.disable()
+    end
   end
 end

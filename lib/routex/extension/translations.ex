912e8dc85c0c54cf3f50962499b9003aca1fa472
Bart Otten <bart.otten85@gmail.com>
2025-10-17 22:01:18 +0200

    chore: enhanced compilation reliability

--- lib/routex/extension/translations.ex
+++ lib/routex/extension/translations.ex
@@ -1,223 +1,211 @@
-defmodule Routex.Extension.Translations do
-  @moduledoc """
-  Enables users to enter URLs using localized terms which can enhance user engagement
-  and content relevance.
+if Code.ensure_loaded?(Gettext) do
+  defmodule Routex.Extension.Translations do
+    @moduledoc """
+    Enables users to enter URLs using localized terms which can enhance user engagement
+    and content relevance.
 
-  Extracts segments of a routes' path to a translations domain file (default: `routes.po`)
-  for translation. At compile-time it combines the translated segments to transform routes.
+    Extracts segments of a routes' path to a translations domain file (default: `routes.po`)
+    for translation. At compile-time it combines the translated segments to transform routes.
 
-  This extension expects either a `:language` attribute or a `:locale` attribute. When only
-  `:locale` is provided it will try to extract the language from the locale tag. This algorithm
-  covers Alpha-2 and Alpha-3 codes (see:
-  [ISO](https://datatracker.ietf.org/doc/html/rfc5646#section-2.2.1))
+    This extension expects either a `:language` attribute or a `:locale` attribute. When only
+    `:locale` is provided it will try to extract the language from the locale tag. This algorithm
+    covers Alpha-2 and Alpha-3 codes (see:
+    [ISO](https://datatracker.ietf.org/doc/html/rfc5646#section-2.2.1))
 
-  This extension requires Gettext >= 0.26.
+    This extension requires Gettext >= 0.26.
 
-  > #### In combination with... {: .neutral}
-  > How to combine this extension for localization is written in de [Localization Guide](guides/LOCALIZE_PHOENIX.md)
+    > #### In combination with... {: .neutral}
+    > How to combine this extension for localization is written in de [Localization Guide](guides/LOCALIZE_PHOENIX.md)
 
-  ## Configuration
-  ```diff
-  defmodule ExampleWeb.RoutexBackend do
-  use Routex.Backend,
-  extensions: [
-    Routex.Extension.AttrGetters, # required
-  + Routex.Extension.Translations
-  ]
-  + translations_backend: MyApp.Gettext,
-  + translations_domain: "routes",
-  ```
-
-  ## Pseudo result
-      # when translated to Spanish in the .po file
-      # - products: producto
-      # - edit: editar
-
-      /products/:id/edit  ⇒ /producto/:id/editar
-
-  ## `Routex.Attrs`
-  **Requires**
-  - language || locale
-
-  **Sets**
-  - none
-
-  ## Use case(s)
-  This extension can be combined with `Routex.Extension.Alternatives` to create
-  multilingual routes.
-
-  Use Alternatives to create new branches and provide a `:language` or `:locale` per branch and
-  Translations to translate the alternative routes.
-
-                          ⇒ /products/:id/edit                  language: "en"
-      /products/:id/edit  ⇒ /nederland/producten/:id/bewerken   language: "nl"
-                          ⇒ /espana/producto/:id/editar         language: "es"
-  """
-
-  @behaviour Routex.Extension
-
-  alias Routex.Attrs
-  alias Routex.Types, as: T
-
-  require Logger
-  require Gettext.Macros
-
-  @separator "/"
-  @interpolate ":"
-  @catch_all "*"
-  @default_domain "routes"
-
-  @impl Routex.Extension
-  @spec configure(T.opts(), T.backend()) :: T.opts()
-  def configure(config, _backend) do
-    gettext_in_compilers? =
-      Mix.Project.get!().project()
-      |> Access.get(:compilers, [])
-      |> Enum.member?(:gettext)
-
-    auto_compile? =
-      Version.match?(System.version(), ">= 1.14.0")
-
-    if !(auto_compile? or gettext_in_compilers?),
-      do:
-        Logger.warning(
-          "When route translations are updated, run `mix compile --force [MyWebApp].Route"
-        )
-
-    default_gettext_mod = lookup_gettext_module()
-
-    [
-      {:translations_domain, Keyword.get(config, :translations_domain, @default_domain)},
-      {:translations_backend, Keyword.get(config, :translations_backend, default_gettext_mod)}
-      | config
+    ## Configuration
+    ```diff
+    defmodule ExampleWeb.RoutexBackend do
+    use Routex.Backend,
+    extensions: [
+      Routex.Extension.AttrGetters, # required
+    + Routex.Extension.Translations
     ]
-  end
+    + translations_backend: MyApp.Gettext,
+    + translations_domain: "routes",
+    ```
 
-  @impl Routex.Extension
-  @spec transform(T.routes(), T.backend(), T.env()) :: T.routes()
-  def transform(routes, config_backend, _env) do
-    config = config_backend.config()
+    ## Pseudo result
+        # when translated to Spanish in the .po file
+        # - products: producto
+        # - edit: editar
 
-    for route <- routes do
-      language = Attrs.get(route, :language)
-      locale = Attrs.get(route, :locale)
+        /products/:id/edit  ⇒ /producto/:id/editar
 
-      detected = language || detect_language!(locale, route)
+    ## `Routex.Attrs`
+    **Requires**
+    - language || locale
 
-      path =
-        translate(route.path, detected, config.translations_backend, config.translations_domain)
+    **Sets**
+    - none
 
-      %{route | path: path}
+    ## Use case(s)
+    This extension can be combined with `Routex.Extension.Alternatives` to create
+    multilingual routes.
+
+    Use Alternatives to create new branches and provide a `:language` or `:locale` per branch and
+    Translations to translate the alternative routes.
+
+                            ⇒ /products/:id/edit                  language: "en"
+        /products/:id/edit  ⇒ /nederland/producten/:id/bewerken   language: "nl"
+                            ⇒ /espana/producto/:id/editar         language: "es"
+    """
+
+    @behaviour Routex.Extension
+
+    alias Routex.Attrs
+    alias Routex.Types, as: T
+
+    require Logger
+    require Gettext.Macros
+
+    @separator "/"
+    @interpolate ":"
+    @catch_all "*"
+    @default_domain "routes"
+
+    @impl Routex.Extension
+    @spec configure(T.opts(), T.backend()) :: T.opts()
+    def configure(config, _backend) do
+      [
+        {:translations_domain, Keyword.get(config, :translations_domain, @default_domain)},
+        {:translations_backend,
+         Keyword.get_lazy(config, :translations_backend, fn -> lookup_gettext_module() end)}
+        | config
+      ]
     end
-  end
 
-  @impl Routex.Extension
-  @spec create_helpers(T.routes(), T.backend(), T.env()) :: T.ast()
-  # creates gettext triggers so route segments are extracted into a translation file
-  def create_helpers(routes, config_backend, _env) do
-    config = config_backend.config()
-    backend = config.translations_backend
-    domain = config.translations_domain
+    @impl Routex.Extension
+    @spec transform(T.routes(), T.backend(), T.env()) :: T.routes()
+    def transform(routes, config_backend, _env) do
+      config = config_backend.config()
 
-    is_original_fn = &(&1 |> Attrs.get(:__branch__) |> List.last() == 0)
+      for route <- routes do
+        language = Attrs.get(route, :language)
+        locale = Attrs.get(route, :locale)
 
-    uniq_segments =
-      routes
-      |> Enum.filter(is_original_fn)
-      |> Enum.map(&Path.split(&1.path))
-      |> List.flatten()
-      |> Enum.uniq()
+        detected = language || detect_language!(locale, route)
 
-    prelude =
-      quote do
-        use Gettext, backend: unquote(backend)
+        path =
+          translate(route.path, detected, config.translations_backend, config.translations_domain)
+
+        %{route | path: path}
       end
+    end
 
-    triggers_ast =
-      Enum.map(uniq_segments, fn
-        @catch_all ->
-          nil
+    @impl Routex.Extension
+    @spec create_helpers(T.routes(), T.backend(), T.env()) :: T.ast()
+    # creates gettext triggers so route segments are extracted into a translation file
+    def create_helpers(routes, config_backend, _env) do
+      config = config_backend.config()
+      backend = config.translations_backend
+      domain = config.translations_domain
 
-        @interpolate <> _rest ->
-          nil
+      is_original_fn = &(&1 |> Attrs.get(:__branch__) |> List.last() == 0)
 
-        segment when not is_binary(segment) ->
-          nil
+      uniq_segments =
+        routes
+        |> Enum.filter(is_original_fn)
+        |> Enum.map(&Path.split(&1.path))
+        |> List.flatten()
+        |> Enum.uniq()
 
-        "/" ->
-          nil
+      prelude =
+        quote do
+          use Gettext, backend: unquote(backend)
+        end
 
-        segment ->
-          quote do:
-                  Gettext.Macros.dgettext_with_backend(
-                    unquote(backend),
-                    unquote(domain),
-                    unquote(segment)
-                  )
-      end)
+      triggers_ast =
+        Enum.map(uniq_segments, fn
+          @catch_all ->
+            nil
 
-    [prelude | triggers_ast]
-  end
+          @interpolate <> _rest ->
+            nil
 
-  defp lookup_gettext_module do
-    lookup_app_module()
-    |> Atom.to_string()
-    |> Kernel.<>("Web.Gettext")
-    |> List.wrap()
-    |> Module.concat()
-  end
+          segment when not is_binary(segment) ->
+            nil
 
-  defp lookup_app_module do
-    Mix.Project.get!()
-    |> Module.split()
-    |> :lists.droplast()
-    |> Module.concat()
-  end
+          "/" ->
+            nil
 
-  defp translate(path, locale, backend, domain)
+          segment ->
+            quote do:
+                    Gettext.Macros.dgettext_with_backend(
+                      unquote(backend),
+                      unquote(domain),
+                      unquote(segment)
+                    )
+        end)
 
-  defp translate(path, locale, backend, domain) when is_binary(path) do
-    Gettext.put_locale(backend, locale)
+      [prelude | triggers_ast]
+    end
 
-    path
-    |> into_segment_list()
-    |> translate_segments(locale, backend, domain)
-    |> Enum.join()
-  end
+    defp lookup_gettext_module do
+      lookup_app_module()
+      |> Atom.to_string()
+      |> Kernel.<>("Web.Gettext")
+      |> List.wrap()
+      |> Module.concat()
+    end
 
-  defp into_segment_list(path), do: Regex.split(~r{(/)}, path, include_captures: true, trim: true)
+    defp lookup_app_module do
+      Mix.Project.get!()
+      |> Module.split()
+      |> :lists.droplast()
+      |> Module.concat()
+    end
 
-  defp translate_segments(segments, locale, backend, domain) do
-    Gettext.put_locale(backend, locale)
-    Enum.map(segments, &translate_segment(&1, locale, backend, domain))
-  end
+    defp translate(path, locale, backend, domain)
 
-  defp translate_segment(@separator, _loc, _back, _domain), do: @separator
-  defp translate_segment(@catch_all, _loc, _back, _domain), do: @catch_all
-  defp translate_segment(@interpolate <> _rest = segment, _loc, _back, _domain), do: segment
-  defp translate_segment(segment, _loc, _back, _domain) when not is_binary(segment), do: segment
+    defp translate(path, locale, backend, domain) when is_binary(path) do
+      Gettext.put_locale(backend, locale)
 
-  defp translate_segment(segment, _loc, backend, domain),
-    do: Gettext.dgettext(backend, domain, segment)
+      path
+      |> into_segment_list()
+      |> translate_segments(locale, backend, domain)
+      |> Enum.join()
+    end
 
-  defp detect_language!(<<lang::binary-size(2)>>, _route), do: lang
-  defp detect_language!(<<lang::binary-size(3)>>, _route), do: lang
-  defp detect_language!(<<lang::binary-size(2), ?-, _rest::binary>>, _route), do: lang
-  defp detect_language!(<<lang::binary-size(3), ?-, _rest::binary>>, _route), do: lang
-  defp detect_language!(<<lang::binary-size(2), ?_, _rest::binary>>, _route), do: lang
-  defp detect_language!(<<lang::binary-size(3), ?_, _rest::binary>>, _route), do: lang
+    defp into_segment_list(path),
+      do: Regex.split(~r{(/)}, path, include_captures: true, trim: true)
 
-  defp detect_language!(nil, route) do
-    backend = route |> Attrs.get(:__backend__) |> to_string()
+    defp translate_segments(segments, locale, backend, domain) do
+      Gettext.put_locale(backend, locale)
+      Enum.map(segments, &translate_segment(&1, locale, backend, domain))
+    end
 
-    raise("Routex backend `#{backend}` lists extension `#{__MODULE__}` but
+    defp translate_segment(@separator, _loc, _back, _domain), do: @separator
+    defp translate_segment(@catch_all, _loc, _back, _domain), do: @catch_all
+    defp translate_segment(@interpolate <> _rest = segment, _loc, _back, _domain), do: segment
+    defp translate_segment(segment, _loc, _back, _domain) when not is_binary(segment), do: segment
+
+    defp translate_segment(segment, _loc, backend, domain),
+      do: Gettext.dgettext(backend, domain, segment)
+
+    defp detect_language!(<<lang::binary-size(2)>>, _route), do: lang
+    defp detect_language!(<<lang::binary-size(3)>>, _route), do: lang
+    defp detect_language!(<<lang::binary-size(2), ?-, _rest::binary>>, _route), do: lang
+    defp detect_language!(<<lang::binary-size(3), ?-, _rest::binary>>, _route), do: lang
+    defp detect_language!(<<lang::binary-size(2), ?_, _rest::binary>>, _route), do: lang
+    defp detect_language!(<<lang::binary-size(3), ?_, _rest::binary>>, _route), do: lang
+
+    defp detect_language!(nil, route) do
+      backend = route |> Attrs.get(:__backend__) |> to_string()
+
+      raise("Routex backend `#{backend}` lists extension `#{__MODULE__}` but
  neither the attribute :language nor :locale was found in private.routex
  of route #{inspect(route, pretty: true)}.")
-  end
+    end
 
-  defp detect_language!(other, route) do
-    raise(
-      ":locale `#{other}` is a non supported format. Found in private.routex of route #{inspect(route, pretty: true)}."
-    )
+    defp detect_language!(other, route) do
+      raise(
+        ":locale `#{other}` is a non supported format. Found in private.routex of route #{inspect(route, pretty: true)}."
+      )
+    end
   end
 end

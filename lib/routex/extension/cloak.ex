41fa9d55a168fa7530d34ecdf846d56320dd8167
Bart Otten <bart.otten85@gmail.com>
2025-10-26 21:20:37 +0100

    feat(cloak): custom functions for route transformation

--- lib/routex/extension/cloak.ex
+++ lib/routex/extension/cloak.ex
@@ -27,7 +27,12 @@ defmodule Routex.Extension.Cloak do
   `Routex.Extension.Alternatives` for even more chaos.
 
   - Adapt this extension to use character repetition instead of numbers. Can you
-  guess where `/90/**` leads to?
+  guess where `/90/!!` brings to?
+
+
+  ## Options
+  - `cloak`: Binary to duplicate or tuple with {module, function, arguments} which will receive a
+  index counter as first argument.
 
 
   ## Configuration
@@ -39,6 +44,7 @@ defmodule Routex.Extension.Cloak do
      Routex.Extension.AttrGetters, # required
   +  Routex.Extension.Cloak
   ],
+  cloak: "!"
   ```
 
   ## Pseudo result
@@ -63,35 +69,57 @@ defmodule Routex.Extension.Cloak do
   @interpolate ":"
   @catch_all "*"
 
+  def default_transform("/", _idx, _opt), do: []
+  def default_transform(_path, idx, _opt), do: [to_string(idx)]
+
+  def binary_transform("/", _idx, _binary), do: []
+  def binary_transform(_path, idx, binary), do: [String.duplicate(binary, idx + 1)]
+
+  def function_transform("/", _idx, _mfa), do: []
+  def function_transform(path, idx, {m, f, a}), do: apply(m, f, [path, idx | a])
+
   @impl Routex.Extension
-  @spec transform(T.routes(), T.backend(), T.env()) :: T.routes()
-  def transform(routes, _backend, _env) do
-    {routes, _cloak_map} =
-      routes
-      |> Enum.with_index()
-      |> Enum.reduce({[], %{}}, fn {route, idx}, {routes, cloak_map} ->
-        if path = cloak_map[route.path] do
-          route = %{route | path: path}
-          {[route | routes], cloak_map}
-        else
-          dynamics =
-            route.path
-            |> Path.split()
-            |> Enum.filter(&(&1 === @catch_all || String.starts_with?(&1, @interpolate)))
+  @spec configure(T.opts(), T.backend()) :: T.opts()
+  def configure(config, _backend) do
+    opt = Keyword.get(config, :cloak)
 
-          static = static_to_segment(idx)
+    transform_mfa =
+      cond do
+        is_binary(opt) -> {__MODULE__, :binary_transform, [opt]}
+        is_tuple(opt) -> {__MODULE__, :function_transform, [opt]}
+        is_nil(opt) -> {__MODULE__, :default_transform, [opt]}
+      end
 
-          path = ["/", dynamics, static] |> Path.join() |> Path.absname()
-          cloak_map = Map.put_new(cloak_map, route.path, path)
-          route = %{route | path: path}
-
-          {[route | routes], cloak_map}
-        end
-      end)
-
-    routes
+    Keyword.put(config, :cloak_transform, transform_mfa)
   end
 
-  defp static_to_segment(0), do: []
-  defp static_to_segment(idx), do: [to_string(idx)]
+  @impl Routex.Extension
+  @spec transform(T.routes(), T.backend(), T.env()) :: T.routes()
+  def transform(routes, backend, _env) do
+    {cm, cf, ca} = backend.config().cloak_transform
+
+    routes
+    |> Enum.with_index()
+    |> Enum.reduce({[], %{}}, fn {route, idx}, {routes, cloak_map} ->
+      if path = cloak_map[route.path] do
+        route = %{route | path: path}
+        {[route | routes], cloak_map}
+      else
+        dynamics =
+          route.path
+          |> Path.split()
+          |> Enum.filter(&(&1 === @catch_all || String.starts_with?(&1, @interpolate)))
+
+        static = apply(cm, cf, [route.path, idx | ca])
+
+        path = ["/", dynamics, static] |> Path.join() |> Path.absname()
+        cloak_map = Map.put_new(cloak_map, route.path, path)
+        route = %{route | path: path}
+
+        {[route | routes], cloak_map}
+      end
+    end)
+    |> elem(0)
+    |> Enum.reverse()
+  end
 end

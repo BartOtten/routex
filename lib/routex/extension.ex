a41a2e6315572c5c4eb92973a9ac487157b411c9
Bart Otten <bart.otten85@gmail.com>
2025-10-27 22:21:58 +0100

    feat: add callback create_shared_helpers/3

--- lib/routex/extension.ex
+++ lib/routex/extension.ex
@@ -34,12 +34,57 @@ defmodule Routex.Extension do
 
   @doc """
   The `create_helpers/3` callback is called in the last stage with a list of
-  routes belonging to a Routex backend, the name of the Routex backend and
-  the current environment. It is expected to return Elixir AST.
+  routes belonging to a Routex backend, the name of the Routex backend and the
+  current environment. It is expected to return Elixir AST.
+
+  This can be used to create dedicated backend. As multiple backends may include
+  the extension, no catchall fallback is allowed. (see:
+  `create_shared_helpers/3`).
+
+  ```elixir
+  def my_fun(%{arg: RoutexBackend.Default}), do: :foo
+  ```
 
   The AST is included in `MyAppWeb.Router.RoutexHelpers`.
   """
   @callback create_helpers(T.routes(), T.backend(), T.env()) :: T.ast()
 
-  @optional_callbacks [configure: 2, transform: 3, post_transform: 3, create_helpers: 3]
+  @doc """
+  The `create_shared_helpers/3` callback is called in the last stage. It differs
+  from `create_helpers/3` as it is called only once per extension with routes
+  combined of all backends using the extension.
+
+  This can be used to create cross-backend (combined) helpers or dedicated
+  backend helpers combined with an additional catch all function head.
+
+  ```elixir
+  def my_fun(%{arg: RoutexBackend.Default}), do: :foo
+  def my_fun(%{arg: RoutexBackend.Other}), do: :bar
+  def my_fun(_), do: :biz
+  ```
+
+  or
+
+  ```elixir
+  def my_fun(socket) do
+    ...some shared code...
+    case socket_to_backend(socket) do
+      %{arg: RoutexBackend.Default} ->  :foo
+      %{arg: RoutexBackend.Other} ->  :bar
+       _ ->  :biz
+    end
+  end
+  ```
+
+  The AST is included in `MyAppWeb.Router.RoutexHelpers`.
+  """
+  @callback create_shared_helpers(T.routes(), [T.backend(), ...], T.env()) :: T.ast()
+
+  @optional_callbacks [
+    configure: 2,
+    transform: 3,
+    post_transform: 3,
+    create_helpers: 3,
+    create_shared_helpers: 3
+  ]
 end
